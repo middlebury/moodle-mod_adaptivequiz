dfc510687cf6f9803e2a7b39c6c7a751c1603870
Adam Franco <afranco@middlebury.edu>
2013-04-29 15:23:02 -0400

    Style fix: Replaced tabs with spaces.
    Tabs found in whitespace. "Use an indent of 4 spaces with no tab characters."
    http://docs.moodle.org/dev/Coding_style#Indentation

--- viewattemptreport.php
+++ viewattemptreport.php
@@ -44,7 +44,7 @@ require_capability('mod/adaptivequiz:viewreport', $context);
 
 $param = array('instance' => $cm->instance, 'userid' => $userid);
 $sql = "SELECT aa.id, aa.userid, aa.uniqueid, aa.attemptstopcriteria, aa.measure, aa.attemptstate, aa.questionsattempted, aa.timemodified, aa.standarderror AS stderror,
-			   a.highestlevel, a.lowestlevel, a.name, aa.timecreated
+               a.highestlevel, a.lowestlevel, a.name, aa.timecreated
           FROM {adaptivequiz_attempt} aa
           JOIN {adaptivequiz} a ON aa.instance = a.id
          WHERE aa.instance = :instance
