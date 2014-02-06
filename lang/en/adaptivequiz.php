7a193bc76be575c920c979214dd080a5715eb9e3
Adam Franco <afranco@middlebury.edu>
2014-02-06 11:15:48 -0500

    Updated strings to consistently use 'Adaptive Quiz' instead of 'Adaptive Test' or other phrases.

--- lang/en/adaptivequiz.php
+++ lang/en/adaptivequiz.php
@@ -14,40 +14,40 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$string['modulenameplural'] = 'Adaptive Test';
-$string['modulename'] = 'Adaptive Test';
-$string['modulename_help'] = 'The adaptive test activity enables a teacher to create tests that efficiently measure the takers\' abilities. Adaptive tests are comprised  of questions selected from the question bank that are tagged with a score of their difficulty. The questions are chosen to match the estimated ability level of the  current test-taker. If the test-taker succeeds on a question, a more challenging question is presented next. If the test-taker answers a question incorrectly, a less-challenging question is presented next. This technique will develop into a sequence of questions converging on the test-taker\'s effective ability level. The test stops when the test-taker\'s ability is determined to the required accuracy.
+$string['modulenameplural'] = 'Adaptive Quiz';
+$string['modulename'] = 'Adaptive Quiz';
+$string['modulename_help'] = 'The Adaptive Quiz activity enables a teacher to create quizes that efficiently measure the takers\' abilities. Adaptive quizes are comprised  of questions selected from the question bank that are tagged with a score of their difficulty. The questions are chosen to match the estimated ability level of the  current test-taker. If the test-taker succeeds on a question, a more challenging question is presented next. If the test-taker answers a question incorrectly, a less-challenging question is presented next. This technique will develop into a sequence of questions converging on the test-taker\'s effective ability level. The quiz stops when the test-taker\'s ability is determined to the required accuracy.
 
 This activity is best suited to determining an ability measure along a unidimensional scale. While the scale can be very broad, the questions must all provide a measure of ability or aptitude on the same scale. In a placement test for example, questions low on the scale that novices are able to answer correctly should also be answerable by experts, while questions higher on the scale should only be answerable by experts or a lucky guess. Questions that do not discriminate between takers of different abilities on will make the test ineffective and may provide inconclusive results.
 
-Questions used in the adaptive test must
+Questions used in the Adaptive Quiz must
 
  * be automatically scored as correct/incorrect
- * be tagged with their difficulty using \'adpq_\' followed by a positive integer that is within the range for the test
+ * be tagged with their difficulty using \'adpq_\' followed by a positive integer that is within the range for the quiz
 
-The adaptive test can be configured to
+The Adaptive Quiz can be configured to
 
  * define the range of question-difficulties/user-abilities to be measured. 1-10, 1-16, and 1-100 are examples of valid ranges.
- * define the precision required before the test is stopped. Often an error of 5% in the ability measure is an appropriate stopping rule
+ * define the precision required before the quiz is stopped. Often an error of 5% in the ability measure is an appropriate stopping rule
  * require a minimum number of questions to be answered
  * require a maximum number of questions that can be answered
 
 This description and the testing process in this activity are based on <a href="http://www.rasch.org/memo69.pdf">Computer-Adaptive Testing: A Methodology Whose Time Has Come</a> by John Michael Linacre, Ph.D. MESA Psychometric Laboratory - University of Chicago. MESA Memorandum No. 69.';
-$string['pluginadministration'] = 'Computer-Adaptive Testing';
-$string['pluginname'] = 'Computer-Adaptive Testing';
-$string['nonewmodules'] = 'No Computer-Adaptive instances found';
+$string['pluginadministration'] = 'Adaptive Quiz';
+$string['pluginname'] = 'Adaptive Quiz';
+$string['nonewmodules'] = 'No Adaptive Quiz instances found';
 $string['adaptivequizname'] = 'Name';
-$string['adaptivequizname_help'] = 'Enter the name of the Computer-Adaptive test instance';
-$string['adaptivequiz:addinstance'] = 'Add a new adaptive test';
-$string['adaptivequiz:viewreport'] = 'View adaptive test reports';
-$string['adaptivequiz:reviewattempts'] = 'Review adaptive test submissions';
-$string['adaptivequiz:attempt'] = 'Attempt adaptive test';
+$string['adaptivequizname_help'] = 'Enter the name of the Adaptive Quiz instance';
+$string['adaptivequiz:addinstance'] = 'Add a new adaptive quiz';
+$string['adaptivequiz:viewreport'] = 'View adaptive quiz reports';
+$string['adaptivequiz:reviewattempts'] = 'Review adaptive quiz submissions';
+$string['adaptivequiz:attempt'] = 'Attempt adaptive quiz';
 $string['attemptsallowed'] = 'Attempts allowed';
 $string['attemptsallowed_help'] = 'The number of times a student may attempt this activity';
 $string['requirepassword'] = 'Required password';
 $string['requirepassword_help'] = 'Students are required to enter a password before beginning their attempt';
 $string['browsersecurity'] = 'Browser security';
-$string['browsersecurity_help'] = 'If "Full screen pop-up with some JavaScript security" is selected the test will only start if the student has a JavaScript-enabled web-browser, the test appears in a full screen popup window that covers all the other windows and has no navigation controls and students are prevented, as far as is possible, from using facilities like copy and paste';
+$string['browsersecurity_help'] = 'If "Full screen pop-up with some JavaScript security" is selected the quiz will only start if the student has a JavaScript-enabled web-browser, the quiz appears in a full screen popup window that covers all the other windows and has no navigation controls and students are prevented, as far as is possible, from using facilities like copy and paste';
 $string['minimumquestions'] = 'Minimum number of questions';
 $string['minimumquestions_help'] = 'The minimum number of questions the student must attempt';
 $string['maximumquestions'] = 'Maximum number of questions';
@@ -67,7 +67,7 @@ $string['formminquestgreaterthan'] = 'Minimum number of questions must be less t
 $string['formlowlevelgreaterthan'] = 'Lowest level must be less than highest level';
 $string['formstartleveloutofbounds'] = 'The starting level must be a number that is inbetween the lowest and highest level';
 $string['standarderror'] = 'Standard Error to stop';
-$string['standarderror_help'] = 'When the amount of error in the measure of the user\'s ability drops below this amount, the test will stop. Tune this value from the default of 5% to require more or less precision in the ability measure';
+$string['standarderror_help'] = 'When the amount of error in the measure of the user\'s ability drops below this amount, the quiz will stop. Tune this value from the default of 5% to require more or less precision in the ability measure';
 $string['formelementdecimal'] = 'Input a decimal number.  Maximum 10 digits long and maximum 5 digits to the right of the decimal point';
 $string['attemptfeedback'] = 'Attempt feedback';
 $string['attemptfeedback_help'] = 'The attempt feedback is displayed to the user once the attempt is finished';
@@ -96,7 +96,7 @@ $string['recentinprogress'] = 'In progress';
 $string['recentcomplete'] = 'Completed';
 $string['functiondisabledbysecuremode'] = 'That functionality is currently disabled';
 $string['enterrequiredpassword'] = 'Enter required password';
-$string['requirepasswordmessage'] = 'To attempt this test you need to know the test password';
+$string['requirepasswordmessage'] = 'To attempt this quiz you need to know the quiz password';
 $string['wrongpassword'] = 'Password is incorrect';
 $string['noattemptrecords'] = 'No attempt records for this student';
 $string['attemptstate'] = 'State of attempt';
