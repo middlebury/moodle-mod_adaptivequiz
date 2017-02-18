37a3ec40949c09a8156271282e35c9b38d77c1c8
Andriy Semenets <semteacher@gmail.com>
2017-02-18 18:19:27 +0200

    bump version number

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017012200;
-$plugin->release = '1.2.3';
+$plugin->version = 2017021000;
+$plugin->release = '1.2.4.1';
 $plugin->requires = 2014051200;
 $plugin->cron = 0;
 $plugin->component = 'mod_adaptivequiz';
